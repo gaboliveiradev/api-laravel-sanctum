7e6009c4cd25660ad8f25278b265b963b23f1af2
gaboliveiradev <g38789138@gmail.com>
2022-09-26 22:08:15 -0300

    criando tabelas no banco de dados com migration + renomenado a user para UserModel

---
+++ database/migrations/2022_09_27_005854_create_products_models_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->integer('inventory')->nullable();
+            $table->decimal('cost', 5, 2)->nullable();
+            $table->decimal('sale', 5, 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
