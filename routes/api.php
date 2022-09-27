8c717e02d3f51893061fec307abc9ca7c75f5129
gaboliveiradev <g38789138@gmail.com>
2022-09-26 23:37:51 -0300

    criando um grupo de rotas e protegendo-as

--- routes/api.php
+++ routes/api.php
@@ -19,10 +19,10 @@ use App\Models\ProductModel;
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
-Route::post('/products', [ProductController::class, 'store']);
-Route::put('/products/{id}', [ProductController::class, 'update']);
-Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Criando um grupo de rotas e protegendo-as || Só acessa a rota se tiver autenticado
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 });
